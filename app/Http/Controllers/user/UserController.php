837bbde2d295100a5b549b5f868cd81ef2de4a40
rifqieizan <rifqi.eizan@gmail.com>
2019-05-25 19:25:15 +0700

    user job apply

--- app/Http/Controllers/user/UserController.php
+++ app/Http/Controllers/user/UserController.php
@@ -91,20 +91,18 @@ class UserController extends Controller
      */
     public function show($id)
     {
-        $job=Jobs::leftJoin('user_jobs','user_jobs.job_id','=','jobs.id')->where('jobs.id',$id)->first();
+        // $job=Jobs::leftJoin('user_jobs','user_jobs.job_id','=','jobs.id')->where('jobs.id',$id)->first();
         
-        // $job= Jobs::find($id);
-        $job->hasRequest = false;
+        $job= Jobs::find($id);
+        // $job->hasRequest = false;
         
-        if($job->user_id==Auth::user()->id){
-            $job->hasRequest = true;
-        }
+        // if($job->user_id==Auth::user()->id){
+        //     $job->hasRequest = true;
+        // }
 
-        $status=UserJob::where('status','0');
-        
 
         
-        return view('user.user_job',compact('job','status'));
+        return view('user.user_job',compact('job'));
     }
 
     /**
